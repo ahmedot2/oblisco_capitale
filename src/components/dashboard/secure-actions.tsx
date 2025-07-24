90a193031eed510174ceda92698c5d79ed4f240f
Ahmed Osman <Ahmedot2@gmail.com>
2025-07-24 17:49:15 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="o

--- src/components/dashboard/secure-actions.tsx
+++ src/components/dashboard/secure-actions.tsx
@@ -1,17 +1,10 @@
 import { Button } from "../ui/button";
-import { Download, MessageCircle } from "lucide-react";
+import { MessageCircle } from "lucide-react";
 import { BriefingModal } from "../modals/briefing-modal";
-import { VaultModal } from "../modals/vault-modal";
 
 export function SecureActions() {
   return (
     <div className="fixed bottom-6 right-6 z-50 flex flex-col gap-3">
-       <VaultModal>
-         <Button variant="outline" className="rounded-full h-14 w-14 p-0 bg-background/50 backdrop-blur-md border-primary/50 text-primary hover:bg-primary/10 hover:text-primary">
-           <Download className="w-6 h-6" />
-           <span className="sr-only">Download Secure Proposal</span>
-        </Button>
-       </VaultModal>
       <BriefingModal>
         <Button className="rounded-full h-14 w-14 p-0">
           <MessageCircle className="w-6 h-6" />
